856602c23aa890728698148e837d7e5701c153dd
Jorge A. Reyes Jr <reyes.junior.27@gmail.com>
2022-12-26 23:03:08 -0800

    Clean up file.

--- src/app/services/ManageAppData.ts
+++ src/app/services/ManageAppData.ts
@@ -6,17 +6,76 @@ import { ILayout } from '../_redux/layout/types';
 import {
 	clearLocalStorage,
 	getLocalStorageKey,
-	saveValueToLocalStorage,
+	setLocalStorageKey,
 } from '../services/ManageLocalStorage';
 
 /* Utilities */
 import {
 	APP_VERSION,
 	APP_SETTINGS_KEY,
+	RELEVANT_APP_SETTINGS_KEYS,
 } from '../utilities/Constants';
 import { isValidInteger } from '../utilities/Utilities';
 
-/* Settings */
+/* Manage App Data */
+export const saveValueToLocalStorage = ({ key, value }: any) => setLocalStorageKey(key, value);
+
+export const deleteValueFromLocalStorage = (key: string) => localStorage.removeItem(key);
+
+export const getRelevantSettingsToPreserve = () => {
+	// We want to check and save any app settings supported apps (that are meant to operate within the same ecosystem).
+	return RELEVANT_APP_SETTINGS_KEYS.map((setting) => {
+		let savedSettingValue = getLocalStorageKey(setting);
+		return {
+			key: setting,
+			value: savedSettingValue,
+		};
+	});
+};
+
+export const savePreservedSettings = (preservedSettings: any) => (
+	preservedSettings.map((setting: any) => (
+		(setting.value)
+			? saveValueToLocalStorage({
+				key: setting.key,
+				value: JSON.parse(setting.value),
+			})
+			: null
+	))
+);
+
+export const createNewAppSettings = () => {
+	// Before we clear the localStorage, check for any relevant app settings we want to preserve.
+	const preservedSettings = getRelevantSettingsToPreserve();
+	clearLocalStorage();
+	savePreservedSettings(preservedSettings);
+
+	return saveValueToLocalStorage({
+		key: APP_SETTINGS_KEY,
+		value: {
+			version: APP_VERSION,
+		},
+	});
+};
+
+export const checkSavedAppSettings = (key: string) => {
+	const { version } = JSON.parse(key);
+	const versionMatches = version === APP_VERSION;
+	const validVersion = isValidInteger(version) && versionMatches;
+
+	if (!validVersion) {
+		clearLocalStorage();
+		createNewAppSettings();
+	};
+};
+
+export const syncLocalStorage = () => {
+	const savedAppSettingsKey = getLocalStorageKey(APP_SETTINGS_KEY);
+	return !savedAppSettingsKey
+		? createNewAppSettings()
+		: checkSavedAppSettings(savedAppSettingsKey);
+};
+
 export const getDefaultSetting = (data: any) => {
 	if (!data) {
 		return null;
@@ -38,28 +97,6 @@ export const updateSetting = (
 	const selectedSetting = settings.filter((setting: any) => setting.name === thisSetting.name);
 	const [first] = selectedSetting;
 	return dispatch(action(first));
-};
-
-export const createNewAppSettings = () => {
-	clearLocalStorage();
-
-	return saveValueToLocalStorage({
-		key: APP_SETTINGS_KEY,
-		value: {
-			version: APP_VERSION,
-		},
-	});
-}
-
-export const checkSavedAppSettings = (key: string) => {
-	const { version } = JSON.parse(key);
-	const versionMatches = version === APP_VERSION;
-	const validVersion = isValidInteger(version) && versionMatches;
-
-	if (!validVersion) {
-		clearLocalStorage();
-		createNewAppSettings();
-	};
 };
 
 export const getSavedAppSettings = () => {
@@ -86,7 +123,3 @@ export const updateAppSettings = (updatedSetting: any) => {
 export const getDefaultTheme = (themes: ITheme[]) => getDefaultSetting(themes);
 
 export const getDefaultLayout = (layouts: ILayout[]) => getDefaultSetting(layouts);
-
-export const getDefaultSpacingDensity = (spacingDensities: any) => getDefaultSetting(spacingDensities);
-
-export const getDefaultFontSize = (fontSizes: any) => getDefaultSetting(fontSizes);
