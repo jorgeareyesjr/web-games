8b94bf68efa151d4e1bc98685cb1f3242fcbc8eb
Jorge A. Reyes Jr <reyes.junior.27@gmail.com>
2022-12-26 20:29:58 -0800

    Add new BrowserRouter and redux-related components to the main index.tsx file.

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client';
+import store from './app/_redux/store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+(() => {
+	try {
+		const container = document.getElementById('o-app-root');
+		const root = createRoot(container!)
+		root.render(
+			<React.StrictMode>
+				<Provider store={store}>
+					<BrowserRouter>
+						<App />
+					</BrowserRouter>
+				</ Provider>
+			</React.StrictMode>
+		);
+	} catch (error) {
+		console.error(error);
+	};
+})();
