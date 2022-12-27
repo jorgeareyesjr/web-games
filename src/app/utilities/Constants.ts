380a015c02d1f8a3c93406b32e1c97be617fefeb
Jorge A. Reyes Jr <reyes.junior.27@gmail.com>
2022-12-26 23:04:16 -0800

    Update the app constants.

--- src/app/utilities/Constants.ts
+++ src/app/utilities/Constants.ts
@@ -1,11 +1,12 @@
 /* App */
-export const APP_VERSION = 1.2;
+export const APP_VERSION = 1.0;
 
 /* LocalStorage */
-export const APP_SETTINGS_KEY = 'APP';
+export const APP_SETTINGS_KEY = 'JR-WEB_GAMES';
+export const RELEVANT_APP_SETTINGS_KEYS = [
+	'JR-GITHUB_PAGE',
+];
 
 /* URLs */
-export const APP_FONT_SIZES_URL = 'fontSizes';
 export const APP_LAYOUTS_URL = 'layouts';
-export const APP_SPACING_DENSITIES_URL = 'spacingDensities';
 export const APP_THEMES_URL = 'themes';
