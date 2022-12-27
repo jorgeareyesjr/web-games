e5d1fb1326c7d9db3a6b35080df7f2e5296205f7
Jorge A. Reyes Jr <reyes.junior.27@gmail.com>
2022-12-26 20:30:45 -0800

    Replace create-react-app template code with newly defined App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { lazy, Suspense } from 'react';
+import Spinner from './app/components/Spinner';
+import './app/styles/index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+// React's lazy and Suspense components are used to support code splitting techniques, in an effort to control resource load prioritization and improve overall load performance.
+export default function App() {
+	// Implement component-based code splitting.
+	const AppSettingsWrapper = lazy(() => import('./app/wrappers/AppSettingsWrapper'));
+	const Router = lazy(() => import('./app/components/Router/Router'));
+	return (
+		<Suspense fallback={
+			<Spinner message={`Initializing...`} />
+		}>
+			<AppSettingsWrapper>
+				<Router />
+			</AppSettingsWrapper>
+		</Suspense>
+	);
+};
